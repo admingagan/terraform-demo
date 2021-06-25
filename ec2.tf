4aeda139f05b8c64cf3fe49de764b6e73870d3f5
Gagandeep Singh <admin.gagan@gmail.com>
2021-06-25 12:39:52 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+region = "us-east-2"
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent      = true
+  name_regex       = "^gagan*"
+  owners           = ["self"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Gagandeep"
+  }
+}
+
